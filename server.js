7407517cf0e5b9ba17750b8f8352b374f1685f19
Samuel Sau <samuel51@usf.edu>
2022-12-12 23:44:59 -0500

    defined endpoints that retrieved matches

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require('express');
-const app = express();
 const PORT = process.env.PORT || 3000;
 const axios = require('axios');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 
-const apiKey = process.env.API_KEY;
-// define a route that fetches data from the League of Legends API
-app.get('/summoner/:summonerName', async (req, res) => {
+const app = express();
+app.use(cors());
+const API_KEY = process.env.API_KEY;
+
+//Routes
+
+//Returns summonerid, accountid, and puuid
+app.get('/:summonerName', async (req, res) => {
 	const summonerName = req.params.summonerName;
-	axios
+
+	await axios
 		.get(
 			`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`,
 			{
 				headers: {
-					"Origin": 'https://developer.riotgames.com',
-					'X-Riot-Token': apiKey,
+					'X-Riot-Token': API_KEY,
 				},
 			}
 		)
@@ -26,6 +31,7 @@ app.get('/summoner/:summonerName', async (req, res) => {
 		});
 });
 
+//Returns rank, summonerName, wins/losses
 app.get('/entries/:summonerid', async (req, res) => {
 	const summonerid = req.params.summonerid;
 	axios
@@ -33,14 +39,55 @@ app.get('/entries/:summonerid', async (req, res) => {
 			`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerid}`,
 			{
 				headers: {
-					"Origin": 'https://developer.riotgames.com',
-					'X-Riot-Token': apiKey,
+					Origin: 'https://developer.riotgames.com',
+					'X-Riot-Token': API_KEY,
 				},
 			}
 		)
 		.then((response) => {
 			res.json(response.data);
-            
+		})
+		.catch((error) => {
+			res.status(500).send(error);
+		});
+});
+
+//Returns upto 20 of the recent summoner's matches using puuid
+app.get('/match-history/:puuid', async (req, res) => {
+	const puuid = req.params.puuid
+	await axios
+		.get(
+			`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20`,
+			{
+				headers: {
+					'X-Riot-Token': API_KEY,
+				},
+			}
+		)
+		.then((response) => {
+			res.json(response.data);
+		})
+		.catch((error) => {
+			res.status(500).send(error);
+		});
+});
+
+// Return data from specific match history 
+app.get('/match-history/:summonerid/:matchid', async (req, res) => {
+	
+	const matchid = req.params.matchid;
+
+	await axios
+		.get(
+			`https://americas.api.riotgames.com/lol/match/v5/matches/${matchid}`,
+			{
+				headers: {
+					'X-Riot-Token': API_KEY,
+				},
+			}
+		)
+		.then((response) => {
+			res.json(response.data);
 		})
 		.catch((error) => {
 			res.status(500).send(error);
